aff00c1c409e6839facfbfe5bffd2e244381c38e
Anned <andy444del@gmail.com>
2026-05-07 02:32:45 -0600

    Actualizar server.js

--- server.js
+++ server.js
@@ -198,6 +198,16 @@ const server = http.createServer((req, res) => {
   }
 
   // Archivos estaticos
+if (urlPath === '/lite') {
+  filePath = path.join(__dirname, 'lite.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+  return;
+}
+
   let filePath = urlPath === '/' || urlPath === '' ? '/index.html' : urlPath;
   filePath = path.join(__dirname, filePath);
 
